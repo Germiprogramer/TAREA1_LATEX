10a01c7960a872ebefefb127b668f11fedcf2f72
Germán Llorente <91720991+Germiprogramer@users.noreply.github.com>
2022-09-20 23:41:51 +0000

    DTW finalizado

--- dynamic_time_warping.py
+++ dynamic_time_warping.py
@@ -1,18 +1,28 @@
+from string import octdigits
 from winreg import HKEY_LOCAL_MACHINE
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-a = pd.read_csv("Access_to_electricity.csv")
+electricidad = pd.read_csv("Access_to_electricity.csv")
+#limpieza de datos
+electricidad = electricidad.fillna(0)
 
-import array as arr
+primer_año = list(electricidad["2010"])
+ultimo_año = list(electricidad["2013"])
 
-a = arr.array("b", [1,2,3])
+primeros_paises_primeraño = []
+primeros_paises_ultimoaño = []
 
-b = arr.array("b", [2,4,5])
+for i in range(5):
+    primeros_paises_primeraño.append(primer_año[i])
+for j in range(7):
+    primeros_paises_ultimoaño.append(ultimo_año[j])
 
-array1 = [1, 2, 3, 4, 4, 4, 3, 2, 1]
-array2 = [1, 3, 4, 4, 2, 1]
+for i in range(len(primeros_paises_primeraño)):
+    primeros_paises_primeraño[i] = round(primeros_paises_primeraño[i],2)
+for i in range(len(primeros_paises_ultimoaño)):
+    primeros_paises_ultimoaño[i] = round(primeros_paises_ultimoaño[i],2)
 
 
 #Funcion para calcular la distancia
@@ -30,11 +40,11 @@ def distancia(x,y):
 
 
 def matriz_distancia(a,b):
-    
+    #elaborar la matriz
     matriz = []
     for i in range(len(b)):
         matriz.append([])
-        for j in range(len(a)-1):
+        for j in range(len(a)):
             matriz[i].append(None)
     
     for i in range(len(a)):
@@ -43,8 +53,8 @@ def matriz_distancia(a,b):
                 (matriz[e])[i] = distancia(a[i],b[e])
             except:
                 pass
-    print(matriz)
 
+    #graficar la matriz
     fig, ax =plt.subplots(1,1)
     data=matriz
     column_labels=a
@@ -55,7 +65,7 @@ def matriz_distancia(a,b):
     plt.show()
     return matriz
 
-matriz_distancia(array1, array2)
+matriz_distancia(primeros_paises_primeraño, primeros_paises_ultimoaño)
 
 def matriz_distancia_acumulada(a,b):
     #repetimos el proceso anterior
@@ -63,7 +73,7 @@ def matriz_distancia_acumulada(a,b):
     matriz = []
     for i in range(len(b)):
         matriz.append([])
-        for j in range(len(a)-1):
+        for j in range(len(a)):
             matriz[i].append(None)
     
     for i in range(len(a)):
@@ -72,7 +82,6 @@ def matriz_distancia_acumulada(a,b):
                 (matriz[e])[i] = distancia(a[i],b[e])
             except:
                 pass
-    print(matriz)
     #añadimos
     
     for i in range(len(a)):
@@ -92,29 +101,51 @@ def matriz_distancia_acumulada(a,b):
     plt.show()
     return matriz
 
-z = matriz_distancia_acumulada(array1,array2)
-
-resultado = (z[0])[0]
-posicion = (z[0])[0]
-for i in range(len(b)):
-    for j in range(len(a)):
+def warping_path(a,b):
+    z = matriz_distancia_acumulada(a,b)
+    resultado = (z[0])[0]
+    i=0
+    j=0
+    total=1
+    
+    while i<=(len(b)+1) or j<=(len(a)+1):
+        print("El camino pasa por la posición ({},{})".format(i,j))
         try:
             if (z[i+1])[j]<(z[i+1])[j+1] and (z[i+1])[j]<(z[i])[j+1]:
                 resultado=resultado+(z[i+1])[j]
-                posicion= (i+1,j)
-            elif (z[i])[j+1]<(z[i+1])[j+1] and (z[i+1])[j]<(z[i+1])[j]:
+                i=i+1
+                j=j
+                total=total+1
+            elif (z[i])[j+1]<(z[i+1])[j+1] and (z[i])[j+1]<(z[i+1])[j]:
                 resultado=resultado+(z[i])[j+1]
-                posicion = (i,j+1)
+                i=i
+                j=j+1
+                total=total+1
             elif (z[i+1])[j+1]<=(z[i])[j+1] and (z[i+1])[j+1]<=(z[i+1])[j]:
                 resultado=resultado+(z[i+1])[j+1]
-                posicion = (i+1,j+1)
+                i=i+1
+                j=j+1
+                total=total+1
             else:
                 resultado=resultado+(z[i+1])[j+1]
-                posicion = (i+1,j+1)
-            print(resultado)
-            print(posicion)
-            
-
+                i=i+1
+                j=j+1
+                total=total+1
         except:
-            pass
-print(resultado)
+            break
+    else:
+        pass
+
+    if i < (len(b)-1):
+        resultado=resultado+(z[i+1])[j]
+        total=total+1
+    if j < (len(a)-1):
+        resultado=resultado+(z[i])[j+1]
+        total=total+1
+
+    distancia_entre_funciones = resultado/total
+    print("La distancia entre las dos muestras de datos es {}".format(distancia_entre_funciones))
+
+    plt.show()
+
+warping_path(primeros_paises_primeraño,primeros_paises_ultimoaño)
