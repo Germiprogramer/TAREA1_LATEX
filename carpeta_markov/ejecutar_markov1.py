7a4d82d7bb2b1b6791226302d80435258690d059
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-10-05 18:36:31 +0200

    commit

---
+++ carpeta_markov/ejecutar_markov1.py
@@ -0,0 +1,8 @@
+from markov1 import *
+
+markov1('China')
+markov1('Germany')
+markov1('Italy')
+markov1('Kyrgyz Republic')
+markov1('Nigeria')
+markov1('Spain')
---
+++ carpeta_markov/ejecutar_markov2.py
@@ -0,0 +1,3 @@
+from markov2 import *
+
+markov2('Germany', 'China', 'Spain')
